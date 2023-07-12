0977e01c8b35e0cfd7c5e6f4735beb2ee6a6dd7f
Buzank <buzankkun@gmail.com>
2023-07-13 00:26:25 +0700

    Change Logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BsChatSquareDots } from 'react-icons/bs'
+import { BiSolidPlaneAlt } from 'react-icons/bi'
 
 
 const Footer = () => {
   return (
     <div className='max-w-[1200px] w-full py-8 m-auto mt-4 border-black border-t-2'>
         <div className='flex items-center m-auto justify-center'>
-        <BsChatSquareDots size={30} className='mr-2'/>
+        <BiSolidPlaneAlt size={30} className='mr-2'/>
         <h1 className='text-xl font-bold text-gray'>RIZKY TRAVEL</h1>
         </div>
     </div>
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { AiFillPhone, AiOutlineClockCircle } from "react-icons/ai";
-import { BsChatSquareDots } from "react-icons/bs";
+import { BiSolidPlaneAlt } from "react-icons/bi";
 
 const TopBar = () => {
   const [currentTime, setCurrentTime] = useState("");
@@ -30,7 +30,7 @@ const TopBar = () => {
   return (
     <div className="flex justify-between items-center px-4 py-2">
       <div className="flex items-center">
-        <BsChatSquareDots
+        <BiSolidPlaneAlt
           size={30}
           className="text-[var(--primary-dark)] mr-2"
         />
